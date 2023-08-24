bc22147d3a0139e04145ccf5bce56bd9ec38628a
Lucas Gabriel <gabif.johann@gmail.com>
2023-08-24 01:06:26 -0300

    eslint

--- react/02-ignite-timer/src/components/Button.styles.ts
+++ react/02-ignite-timer/src/components/Button.styles.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -15,4 +15,4 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 
   background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
-`;
+`
