5ad701c1797cf1bb31599b838475e11dc7522479
Lucas Gabriel <gabif.johann@gmail.com>
2023-08-25 23:17:40 -0300

    home page finished

--- react/02-ignite-timer/src/pages/Home/index.tsx
+++ react/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import { Play } from 'phosphor-react'
 import {
-  CountDownContainer,
+  CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,25 +15,33 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
           <label htmlFor="task">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
           <span>minuos.</span>
         </FormContainer>
 
-        <CountDownContainer>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
           <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
-        </CountDownContainer>
-        <button type="submit">
-          Começar
+        </CountdownContainer>
+        <StartCountdownButton type="submit">
           <Play size={24} />
-        </button>
+          Começar
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
