a0228ffa9797830a93693f704a565d722e1bc71c
Lucas Gabriel <gabif.johann@gmail.com>
2023-08-25 22:27:44 -0300

    header & layout

--- react/02-ignite-timer/src/layouts/DefaultLayout/index.tsx
+++ react/02-ignite-timer/src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/* conteúdo específico da página */}
-    </>
+    </LayoutContainer>
   )
 }
