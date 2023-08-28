0956f98500769c8053bf3f767a32032ae8e025a2
Lucas Gabriel <gabif.johann@gmail.com>
2023-08-27 21:39:01 -0300

    improved inputs

--- react/02-ignite-timer/src/pages/Home/index.tsx
+++ react/02-ignite-timer/src/pages/Home/index.tsx
@@ -18,14 +18,23 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="estudar" />
+            <option value="trabalhar" />
+            <option value="tomar remédios" />
+          </datalist>
 
           <label htmlFor="task">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5} /* setas desabilitadas no CSS */
+            min={5}
+            max={60}
           />
 
           <span>minuos.</span>
