702d3caef716ce80d6c41ebe7d014af15ff1ac26
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-05 00:04:04 -0300

    format date

--- react/02-ignite-timer/src/pages/History/History.tsx
+++ react/02-ignite-timer/src/pages/History/History.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
-import { HistoryContainer, HistoryList, Status } from './styles'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { CyclesContext } from '../../contexts/CyclesContexts'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -25,7 +28,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td> {cycle.task}</td>
                   <td> {cycle.minutesAmount} minutos</td>
-                  <td> {cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
