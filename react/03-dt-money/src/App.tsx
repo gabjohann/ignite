539aaae7ef3cec56f801f68faa54883c8ef205a8
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-17 23:47:28 -0300

    header component

--- react/03-dt-money/src/App.tsx
+++ react/03-dt-money/src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styes/themes/defaults';
 import { GlobalStyle } from './styes/global';
+import { Transactions } from './pages/Transactions';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>oi</h1>
+
+      <Transactions />
     </ThemeProvider>
   );
 }
