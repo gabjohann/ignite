5360e2a02584c8a6b4170635281cc0761d557044
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-18 00:49:18 -0300

    transactions table

--- react/03-dt-money/src/pages/Transactions/index.tsx
+++ react/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from '../../../components/Header';
 import { Summary } from '../../../components/Summary';
+import {
+  PriceHightLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export function Transactions() {
   return (
     <main>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <td width='50%'>Desenvolvimento de site</td>
+            <td>
+              <PriceHightLight variant='income'>R$ 12.000,00</PriceHightLight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tbody>
+
+          <tbody>
+            <td width='50%'>Hambúrguer</td>
+            <td>
+              <PriceHightLight variant='outcome'>- R$ 59,00</PriceHightLight>
+            </td>
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </main>
   );
 }
