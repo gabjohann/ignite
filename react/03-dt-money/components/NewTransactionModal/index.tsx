2f9a88f0600be33a3ca5666cfe7ca39efea26b5b
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-20 18:37:57 -0300

    add: json server

--- react/03-dt-money/components/NewTransactionModal/index.tsx
+++ react/03-dt-money/components/NewTransactionModal/index.tsx
@@ -14,23 +14,23 @@ export function NewTransactionModal() {
     <Dialog.Portal>
       <Overlay />
       <Content>
-        <Dialog.Title>Nova transação</Dialog.Title>
+        <Dialog.Title>Nova Transação</Dialog.Title>
         <CloseButton>
           <X size={24} />
         </CloseButton>
 
         <form action=''>
           <input type='text' placeholder='Descrição' required />
-          <input type='text' placeholder='Preço' required />
+          <input type='number' placeholder='Preço' required />
           <input type='text' placeholder='Categoria' required />
 
           <TransactionType>
-            <TransactionTypeButton variant='income'>
+            <TransactionTypeButton variant='income' value='income'>
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
 
-            <TransactionTypeButton variant='outcome'>
+            <TransactionTypeButton variant='outcome' value='outcome'>
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
