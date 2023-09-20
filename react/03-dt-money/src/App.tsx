2cf55e2c97dbab8755a4c98c708e596b7ae3a235
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-20 19:06:09 -0300

    create transactions context

--- react/03-dt-money/src/App.tsx
+++ react/03-dt-money/src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styes/themes/defaults';
 import { GlobalStyle } from './styes/global';
 import { Transactions } from './pages/Transactions';
+import { TransactionsProvider } from './contexts/TransactionsContext';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
