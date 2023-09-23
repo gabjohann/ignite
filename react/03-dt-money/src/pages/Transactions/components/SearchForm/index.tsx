4168ecc9479bf3197bfee28e9965a1d0323c2869
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-22 21:40:03 -0300

    add: memo example

--- react/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ react/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -6,12 +6,33 @@ import { useContextSelector } from 'use-context-selector';
 import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 import { SearchFormContainer } from './style';
 
+/**
+ * Por que um componente renderiza?
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades);
+ * Parent rerendered (componente pai renderizou);
+ * 
+ * Qual o fluxo de renderização?
+* 1. O React recria o HTML da interface daquele componente
+* 2. Compara a versão do HTML recriada com a versão anterior
+* 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+* 
+* Memo:
+
+* 0. Hooks changed | Props changed (deep comparison) 
+* 0.1 Comparar com a versão 0.1 dos hooks e forms
+* 0.2 SE mudou algo, ele vai permitir a nova renderização
+*/
+
 const searchFormSchema = z.object({
   query: z.string(),
 });
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
+// Exemplo de uso
+// import { memo } from 'react';
+//function SearchFormComponent() {
 export function SearchForm() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
@@ -47,3 +68,6 @@ export function SearchForm() {
     </SearchFormContainer>
   );
 }
+
+// Exemplo de uso
+//export const SearchForm = memo(SearchFormComponent);
