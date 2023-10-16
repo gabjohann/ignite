bdfbd92dd6e7ef28bcd966ef0fef1de8d61ea381
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-16 17:19:19 -0300

    add: carousel

--- react/04-ignite-shop/src/pages/index.tsx
+++ react/04-ignite-shop/src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image';
 import { HomeContainer, Product } from '../styles/pages/home';
 
+import { useKeenSlider } from 'keen-slider/react';
+
 import camiseta1 from '../assets/camiseta_1.png';
 import camiseta2 from '../assets/camiseta_2.png';
 import camiseta3 from '../assets/camiseta_3.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta1} alt='' width={520} height={480} />
 
         <footer>
@@ -17,11 +28,20 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta2} alt='' width={520} height={480} />
 
         <footer>
           <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} alt='' width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Z</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
