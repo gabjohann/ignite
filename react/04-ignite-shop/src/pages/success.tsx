e7f43b34dcefe42a39369848f80da08743746ec0
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-23 21:12:08 -0300

    add: success data

--- react/04-ignite-shop/src/pages/success.tsx
+++ react/04-ignite-shop/src/pages/success.tsx
@@ -1,20 +1,56 @@
 import Link from 'next/link';
-import { SuccessContainer, ImageContainer } from '../styles/pages/success';
+import Image from 'next/image';
+import { GetServerSideProps } from 'next';
 
-export default function Success() {
+import Stripe from 'stripe';
+import { stripe } from '../lib/stripe';
+import { SuccessContainer, ImageContainer } from '../styles/pages/success';
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt='' />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Lucas Johann</strong>, sua{' '}
-        <strong>Camiseta Beyound the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{customerName}</strong>, sua
+        <strong> {product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href='/'>Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
+
+  const customerName = session.customer_details.name;
+
+  // 0 pois o ecomerce só permite 1 compra por vez
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
