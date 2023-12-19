fa662bf8d3a93105745497a863ec147715c0de57
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-19 18:28:30 -0300

    add: api routes

---
+++ react/desafios/book-wise/src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,220 @@
+import { NextApiResponse, NextPageContext } from 'next'
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(
+  req: NextApiResponse | NextPageContext['req'],
+  res: NextApiResponse | NextPageContext['res'],
+): Adapter {
+  return {
+    async createUser(user) {
+      const prismaUser = await prisma.user.create({
+        data: {
+          name: user.name!,
+          avatar_url: user.image,
+        },
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: '',
+        avatar_url: prismaUser.avatar_url,
+        emailVerified: null,
+        ratings: [],
+      }
+    },
+
+    async getUser(id) {
+      const user = await prisma.user.findUnique({
+        where: { id },
+        include: {
+          ratings: {
+            select: {
+              book_id: true,
+            },
+          },
+        },
+      })
+
+      if (!user) {
+        return null
+      }
+
+      return {
+        id: user.id,
+        name: user.name,
+        avatar_url: user.avatar_url || '',
+        email: '',
+        emailVerified: null,
+        ratings: user.ratings.map((rate) => {
+          return rate.book_id
+        }),
+      }
+    },
+
+    async getUserByEmail(email) {
+      return null
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      const account = await prisma.account.findUnique({
+        where: {
+          provider_provider_account_id: {
+            provider_account_id: providerAccountId,
+            provider,
+          },
+        },
+        include: {
+          user: {
+            include: {
+              ratings: true,
+            },
+          },
+        },
+      })
+
+      if (!account) {
+        return null
+      }
+
+      const { user } = account
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: '',
+        avatar_url: user.avatar_url,
+        emailVerified: null,
+        ratings: user.ratings.map((rate) => {
+          return rate.book_id
+        }),
+      }
+    },
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name!,
+          avatar_url: user.image!,
+        },
+        include: {
+          ratings: true,
+        },
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: '',
+        avatar_url: prismaUser.avatar_url,
+        emailVerified: null,
+        ratings: prismaUser.ratings.map((rate) => {
+          return rate.book_id
+        }),
+      }
+    },
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      })
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          session_token: sessionToken,
+          user_id: userId,
+          expires,
+        },
+      })
+
+      return {
+        sessionToken,
+        userId,
+        expires,
+      }
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const prismaSession = await prisma.session.findFirstOrThrow({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: {
+            include: {
+              ratings: true,
+            },
+          },
+        },
+      })
+
+      if (!prismaSession) {
+        return null
+      }
+
+      const { user, ...session } = prismaSession
+
+      return {
+        session: {
+          userId: user.id,
+          expires: session.expires,
+          sessionToken: session.session_token,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          email: '',
+          avatarUrl: user.avatar_url,
+          emailVerified: null,
+          ratings: user.ratings.map((rate) => {
+            return rate.book_id
+          }),
+        },
+      }
+    },
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      })
+
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
+
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken,
+        },
+      })
+    },
+  }
+}
